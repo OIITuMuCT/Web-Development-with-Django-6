d6815bc926e6b5c199aa196ee685839bfa88d30d
OIITuMuCT <vataman@live.ru>
2026-05-04 14:12:39 +0300

    add settings: postgresql config

--- bookr/bookr/settings.py
+++ bookr/bookr/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "bookr.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bookr",
+        "USER": "django",
+        "PASSWORD": "wCh29&HE&T830b3ef4b0d80c28db",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
