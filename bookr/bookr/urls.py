b44ad651b84ec01f8baaeaaf3a216ef23963c4c7
OIITuMuCT <vataman@live.ru>
2026-05-14 15:27:02 +0300

    build(media_project): add media config

--- bookr/bookr/urls.py
+++ bookr/bookr/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 import reviews.views
@@ -25,3 +26,7 @@ urlpatterns = [
     path("", include("reviews.urls")),
     path("book-search/", reviews.views.book_search, name="book-search"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT)
