d63968b0c26375ce3cd0b3eaa61c069e41ab45d5
OIITuMuCT <vataman@live.ru>
2026-05-14 19:27:07 +0300

    feat(media_project): image uploads using django forms

--- media_project/media_example/views.py
+++ media_project/media_example/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import redirect, render
 from django.conf import settings
-
+from PIL import Image
 from .forms import UploadForm
 
 
 def media_example(request):
+
     if request.method == "POST":
         form = UploadForm(request.POST, request.FILES)
         if form.is_valid():
             save_path = (settings.MEDIA_ROOT /
                 request.FILES['file_upload'].name)
-
-            with open(save_path, "wb") as output_file:
-                for chunk in form.cleaned_data['file_upload'].chunks():
-                    output_file.write(chunk)
+            image = Image.open(form.cleaned_data['file_upload'])
+            image.thumbnail((50, 50))
+            image.save(save_path)
+            # with open(save_path, "wb") as output_file:
+            #     for chunk in form.cleaned_data['file_upload'].chunks():
+            #         output_file.write(chunk)
     else:
         form = UploadForm()
     return render(request, "media-example.html", {"form": form})
-
 
