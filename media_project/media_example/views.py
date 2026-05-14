33fffd4a4a9a6315f669d1e32f2501c2e104188c
OIITuMuCT <vataman@live.ru>
2026-05-14 21:16:33 +0300

    core(media_project): add FileField and ImageField on models

--- media_project/media_example/views.py
+++ media_project/media_example/views.py
@@ -1,23 +1,22 @@
+import os.path
 from django.shortcuts import redirect, render
+from django.http import FileResponse
+from django.core.exceptions import PermissionDenied
 from django.conf import settings
 from PIL import Image
 from .forms import UploadForm
+from .models import ExampleModel
 
 
 def media_example(request):
-
     if request.method == "POST":
+        instance = None
         form = UploadForm(request.POST, request.FILES)
         if form.is_valid():
-            save_path = (settings.MEDIA_ROOT /
-                request.FILES['file_upload'].name)
-            image = Image.open(form.cleaned_data['file_upload'])
-            image.thumbnail((50, 50))
-            image.save(save_path)
-            # with open(save_path, "wb") as output_file:
-            #     for chunk in form.cleaned_data['file_upload'].chunks():
-            #         output_file.write(chunk)
+            instance = ExampleModel()
+            instance.image_field = form.cleaned_data['image_upload']
+            instance.file_field = form.cleaned_data['file_upload']
+            instance.save()
     else:
         form = UploadForm()
-    return render(request, "media-example.html", {"form": form})
-
+    return render(request, "media-example.html", {"form": form, "instance": instance}, )
