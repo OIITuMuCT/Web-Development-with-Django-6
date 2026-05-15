cf4a8d7f63aa08b0e8f303f64e764562724f5cb6
OIITuMuCT <vataman@live.ru>
2026-05-15 18:25:37 +0300

    chore: add profile.html

--- auth_project/authentication/views.py
+++ auth_project/authentication/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
 
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def view(request):
     return render(request, "base.html", {"message": "Hello World!"})
+
+@login_required
+def profile(request):
+    return render(request, "profile.html")
