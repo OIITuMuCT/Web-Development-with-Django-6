01b25dcc86e5eabb8c77e89ae452bb0cae1ed736
OIITuMuCT <vataman@live.ru>
2026-05-19 19:06:48 +0300

    (reviews): add sessioninfo, search-results.html

--- bookr/reviews/forms.py
+++ bookr/reviews/forms.py
@@ -1,17 +1,19 @@
 from django import forms
 from .models import Publisher, Review
 
+
 class SearchForm(forms.Form):
     search = forms.CharField(required=False, min_length=3)
     search_in = forms.ChoiceField(
         required=False, choices=(("title", "Title"), ("contributor", "Contributor"))
     )
 
+
 class PublisherForm(forms.ModelForm):
     class Meta:
         model = Publisher
         fields = "__all__"
-        
+
 
 class ReviewForm(forms.ModelForm):
     class Meta:
